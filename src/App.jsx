2254b4441c32d3e7e672dc784fe6a47741c69b06
Aayush Ghimire <aausgh@gmail.com>
2024-10-30 22:20:39 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../src/components/navbar'
+
+const App = () => {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
